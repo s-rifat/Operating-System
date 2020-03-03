42bdbef0320a8ec4bbb29dd35fed2b2ccd310871
Syed Rifat <gtr06.rft@gmail.com>
2020-03-04 00:02:57 +0600

    Priority Non Preemptive Easy Version

---
+++ Priority Scheduling Algorithm Non Preemptive Arrival Time Zero( Easy Version).cpp
@@ -0,0 +1,51 @@
+///for Arrival time zero
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+    string process;
+    double arrivaltime,cputime,priority,waitingtime,turnaroundtime,starttime,finishtime;
+    int id;
+} arr[50];
+bool compare(node a, node b)
+{
+    if(a.priority== b.priority)
+        return a.id<b.id;
+    return a.priority<b.priority;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i].process>>arr[i].arrivaltime>>arr[i].cputime>>arr[i].priority;
+        arr[i].id = i;
+    }
+    sort(arr,arr+n,compare);
+    arr[0].turnaroundtime = arr[0].cputime;
+    arr[0].finishtime = arr[0].cputime;
+    double totalwait = 0, totalturn = arr[0].turnaroundtime;
+    for(int i=1;i<n;i++)
+    {
+        arr[i].starttime =arr[i-1].finishtime;
+        arr[i].finishtime = arr[i].starttime+arr[i].cputime;
+        arr[i].waitingtime = arr[i].starttime;
+        arr[i].turnaroundtime = arr[i].waitingtime+arr[i].cputime;
+        totalwait+=arr[i].waitingtime;
+        totalturn+= arr[i].turnaroundtime;
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i].process<<": waiting time: "<<arr[i].waitingtime<<" turnaround time:"<<arr[i].turnaroundtime<<endl;
+    }
+    cout<<"\nAverage wait: "<<totalwait/n<<" Average turn: "<<totalturn/n<<endl;
+    return 0;
+}
+/*
+3
+p1 0 5 0
+p2 0 7 2
+p3 0 9 1
+*/
+
