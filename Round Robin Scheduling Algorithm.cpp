1d1c0b2c905e05972a4ea1cb9c7d510d5eca7e7a
Syed Rifat <gtr06.rft@gmail.com>
2020-03-04 20:30:53 +0600

    Round Robin Scheduling Algorithm Original

--- Round Robin Scheduling Algorithm.cpp
+++ Round Robin Scheduling Algorithm.cpp
@@ -3,8 +3,7 @@ using namespace std;
 struct node
 {
     string process;
-    double cputime,initialcputime,arrivaltime,waitingtime,turnaroundtime;
-    vector<double>starttime,finishtime;
+    double cputime,ct,arrivaltime,At,waitingtime,turnaroundtime;
     int id,index,pushed;
 } arr[50];
 bool compare(node a, node b)
@@ -17,13 +16,16 @@ int main()
 {
     int n;
     double timequantum,time,spend, totalwait = 0, totalturn = 0;
-    node current;
-    queue<node>q;
+    vector<string>v;
+    int current;
+    queue<int>q;
     cin>>n;
     for(int i=0;i<n;i++)
     {
         cin>>arr[i].process>>arr[i].arrivaltime>>arr[i].cputime;
-        arr[i].initialcputime = arr[i].cputime;
+        arr[i].ct = arr[i].cputime;
+        arr[i].At = arr[i].arrivaltime;
+        arr[i].turnaroundtime = arr[i].cputime;
         arr[i].id = i;
     }
     cin>>timequantum;
@@ -33,31 +35,30 @@ int main()
         arr[i].index = i;
     }
     arr[0].pushed = 1;
-    q.push(arr[0]);
+    q.push(arr[0].index);
     time = arr[0].arrivaltime;
     while(!q.empty())
     {
         current = q.front();
         q.pop();
-        current.pushed = 0;
-        arr[current.index].pushed = 0;
+        v.push_back(arr[current].process);
+        arr[current].pushed = 0;
 
-        spend= min(current.cputime,timequantum);
-        current.cputime -= spend;
-        arr[current.index].cputime -= spend;
+        spend= min(arr[current].cputime,timequantum);
+        arr[current].cputime -= spend;
 
-        time = max(current.arrivaltime,time);
-        arr[current.index].starttime.push_back(time);
-        arr[current.index].finishtime.push_back(time+spend);
+        time = max(arr[current].arrivaltime,time);
+        arr[current].waitingtime+=time-arr[current].At;
         time+=spend;
+        arr[current].At = time;
 
-        if(current.cputime==0)
+        if(arr[current].cputime==0)
         {
             for(int i=0;i<n;i++)
             {
                 if(arr[i].cputime!=0 && arr[i].pushed==0)
                 {
-                    q.push(arr[i]);
+                    q.push(arr[i].index);
                     arr[i].pushed = 1;
                     break;
                 }
@@ -67,27 +68,27 @@ int main()
         {
             for(int i=0;i<n;i++)
             {
-                if(arr[i].cputime!=0 && arr[i].arrivaltime<=time && arr[i].pushed==0 && i!=current.index )
+                if(arr[i].cputime!=0 && arr[i].arrivaltime<=time && arr[i].pushed==0 && i!=current )
                 {
-                    q.push(arr[i]);
+                    q.push(arr[i].index);
                     arr[i].pushed = 1;
                 }
             }
-            q.push(current);
-            arr[current.index].pushed = 1;
+            q.push(arr[current].index);
+            arr[current].pushed = 1;
         }
     }
     for(int i=0;i<n;i++)
     {
-        arr[i].waitingtime = arr[i].starttime[0] - arr[i].arrivaltime;
-        for(int j=1;j<arr[i].starttime.size();j++)
-        {
-             arr[i].waitingtime += arr[i].starttime[j]-arr[i].finishtime[j-1];
-        }
-        arr[i].turnaroundtime = arr[i].waitingtime + arr[i].initialcputime;
+        arr[i].turnaroundtime+=arr[i].waitingtime;
         totalwait+= arr[i].waitingtime;
         totalturn+= arr[i].turnaroundtime;
     }
+    for(int i=0;i<v.size();i++)
+    {
+       cout<<v[i]<<" ";
+    }
+    cout<<endl;
     for(int i=0;i<n;i++)
     {
         cout<<arr[i].process<<": Waiting time: "<<arr[i].waitingtime<<" turnaround time: "<<arr[i].turnaroundtime<<endl;
@@ -95,7 +96,6 @@ int main()
     cout<<"Average waiting time: "<<totalwait/n<<"\nAverage turnaround time: "<<totalturn/n<<endl;
     return 0;
 }
-
 /*
 4
 p1 4 5
@@ -104,6 +104,7 @@ p3 6 9
 p4 10 9
 3
 */
+
 
 
 
